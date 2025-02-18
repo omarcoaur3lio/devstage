51a9dff2ac68a9ccf01e1c26a350dde2ee1ee51c
Marco <marco@crabki.com>
2025-02-18 16:07:12 -0300

    :sparkles: change input component to use composite pattern

--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -1,20 +1,25 @@
 import { Button } from "@/components/button";
 import { IconButton } from "@/components/icon-button";
-import { Input } from "@/components/input";
-import { ArrowRight, Copy } from 'lucide-react'
+import { InputField, InputIcon, InputRoot } from "@/components/input";
+
+import { ArrowRight, Copy, Mail } from "lucide-react";
 
 export default function Home() {
   return (
     <main>
       <Button>
-          Enviar
-          <ArrowRight />
-        </Button>
-        <IconButton>
-          <Copy />
-        </IconButton>
-        <Input placeholder="Digite seu e-mail" />
-        <Input placeholder="Digite seu e-mail" error />
+        Enviar
+        <ArrowRight />
+      </Button>
+      <IconButton>
+        <Copy />
+      </IconButton>
+      <InputRoot>
+        <InputIcon>
+          <Mail />
+        </InputIcon>
+        <InputField placeholder="Digite o e-mail" />
+      </InputRoot>
     </main>
   );
 }
