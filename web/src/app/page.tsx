13113b3d8e59a68c2f7c8d711a5a4bc8aed3a0f9
Marco <marco@crabki.com>
2025-02-18 13:18:01 -0300

    :sparkles: add buttons components

--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -1,7 +1,17 @@
+import { Button } from "@/components/button";
+import { IconButton } from "@/components/icon-button";
+import { ArrowRight, Copy } from 'lucide-react'
+
 export default function Home() {
   return (
     <main>
-      <h1 className="text-3xl font-bold underline">      Hello world!    </h1>
+      <Button>
+          Enviar
+          <ArrowRight />
+        </Button>
+        <IconButton>
+          <Copy />
+        </IconButton>
     </main>
   );
 }
